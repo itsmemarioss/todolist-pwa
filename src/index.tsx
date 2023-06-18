846325ad0fb517705af293cd6b1dc6747e1db5d8
Mario Sousa <m_santos_sousa@hotmail.com>
2023-06-18 11:39:49 +0100

    add material ui

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import StickyFooter from './layout/StickyFooter';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
